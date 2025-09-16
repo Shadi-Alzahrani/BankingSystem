979ea3caf294aec7f0eee5dfd0a1cc087ff7a3de
shadi Al-Zahrani <shadi_alzahrani@outlook.sa>
2025-09-16 16:08:18 +0300

    i added the c# project Files

---
+++ Project/BankingSystem_Business/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankingSystem_DataAccess;
+namespace BankingSystem_Business
+{
+    public class Class1
+    {
+      
+    }
+}
